e591131c4dff8eecb945fc6554f5face753642ed
alamincsme <alamincsme@gmail.com>
2024-03-08 14:35:10 +0600

    Java Script object class

--- object_class.js
+++ object_class.js
@@ -1,32 +1,32 @@
-var arr = [1,2,3,4,5];
+var arr = [1, 2, 3, 4, 5];
 for (const x of arr) console.log(x + " ")
 
 
 
 console.log("another part..........")
-class  Product  {
-    constructor(name, description , price ) {
-        this.name = name ;
-        this.description = description ;
-        this.price = price ;
+class Product {
+    constructor(name, description, price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
     }
 }
 
-var  product = new Product("Laptop", "Hp core i 3 11 Gen", 85000.00)
+var product = new Product("Laptop", "Hp core i 3 11 Gen", 85000.00)
 console.log(product)
 
 
 var product = {
-    name :"laptop", 
-    des  : "Good product", 
-    price : 650.00
+    name: "laptop",
+    des: "Good product",
+    price: 650.00
 
 }
 
- var var_keys = Object.keys(product)
- var var_val = Object.values(product)
+var var_keys = Object.keys(product)
+var var_val = Object.values(product)
 
-for (var i = 0; i < var_keys.length ; i++) {
+for (var i = 0; i < var_keys.length; i++) {
     console.log(var_keys[i] + " " + var_val[i]);
 }
 
@@ -35,16 +35,30 @@ for (var i = 0; i < var_keys.length ; i++) {
 console.log(product.name)
 
 const products = [
-        { id : 1 , name :'Xiaomi phone note pro", price ', price : 2500.00}, 
-        { id : 2 , name :'Samsung phone note 6", price ', price : 2500.00}, 
-        { id : 3 , name :'Dell inspriation ", price ', price : 2500.00}, 
-        { id : 4 , name :'Hp corei 3 ", price ', price : 2500.00}, 
-        { id : 5 , name :'Lenovo 45Lg", price ', price : 2500.00}, 
-        { id : 6 , name :'IPhone pro", price ', price : 2500.00}, 
+    { id: 1, name: 'Xiaomi phone note pro", price ', price: 2500.00 },
+    { id: 2, name: 'Samsung phone note 6", price ', price: 2500.00 },
+    { id: 3, name: 'Dell inspriation ", price ', price: 2500.00 },
+    { id: 4, name: 'Hp corei 3 ", price ', price: 2500.00 },
+    { id: 5, name: 'Lenovo 45Lg", price ', price: 2500.00 },
+    { id: 6, name: 'IPhone pro", price ', price: 2500.00 },
 
 ]
 
 
-for (const product of products) {
-    console.log(product)
+// for (const product of products) {
+//     console.log(product)
+// }
+
+function matchedProducts(protucts, search) {
+    const matched = [];
+    for (var product of products) {
+        if (product.name.toLowerCase().includes(search.toLowerCase())) {
+            matched.push(product);
+        }
+    }
+
+    return matched;
 }
+
+const ans = matchedProducts(products, 'phone')
+console.log(ans)
